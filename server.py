2880015f7664dec02610456815d2ab606e610e75
thehummingbird <sharadmaheshwari19@gmail.com>
2023-08-10 13:09:15 +0200

    feat: add basic server functionality

---
+++ server.py
@@ -0,0 +1,39 @@
+# Add these in requirements.txt
+# sentence_transformers==2.2.2
+# flask==2.3.2
+# werkzeug==2.3.6
+
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+
+from privateGPT import PrivateGPT
+
+app = Flask(__name__)
+
+privateGPT = PrivateGPT()
+
+
+@app.route("/ask", methods=['GET'])
+def query():
+  print("Query initialized")
+  question = request.args.get("query")
+  answer = privateGPT.process_query(question)
+  return f'Question is {answer}'
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+  print("file upload started")
+  file = request.files['file']
+  file.save(f"./source_documents/{secure_filename(file.filename)}")
+  print("file upload complete")
+  privateGPT.update_model()
+  print("model OK")
+  return "Ok"
+
+# class PrivateGPT:
+#   def process_query(question):
+#     return "good answer"
+
+#   def update_model():
+#     return "Ok"l
